00ff30b25297e3c0d350731e3fd3e0a5db4fcc52
gindaa <conan.peace@gmail.com>
2016-12-04 23:53:06 +0700

    Add files via upload

--- ASD.cpp
+++ ASD.cpp
@@ -1,4 +1,5 @@
 #include "ASD.h"
+#include <cstdlib>
 
 void createaccount(account &a)
 {
@@ -73,4 +74,164 @@ void printinfo(account &a)
         }
         cout << endl;
     }
+}
+
+void creategame (game &g)
+{
+    g.first = NULL;
+}
+
+elmgame *alokasigame(infotypegame x)
+{
+    elmgame *p = new elmgame;
+    p->profilgame = x;
+    p->next = NULL;
+    return p;
+}
+
+void insertgame (game &g, elmgame *p)
+{
+    elmgame *cari = g.first;
+    if (g.first == NULL)
+    {
+        g.first = p;
+        cout << "Game Berhasil dibuat" << endl;
+    }
+    else if(cari->next == NULL)
+    {
+        if ((p->profilgame.id).compare(cari->profilgame.id) != 0)
+        {
+            p->next = g.first;
+            g.first = p;
+            ;cout << "Game Berhasil dibuat" << endl;
+        }
+        else
+        {
+            ;cout << "Id Sudah Ada" << endl;
+        }
+    }
+    else
+    {
+        elmgame *r = g.first;
+        while((r->next != NULL) and ((p->profilgame.id).compare(r->profilgame.id) != 0))
+        {
+            r = r->next;
+        }
+        if ((p->profilgame.id).compare(r->profilgame.id) == 0)
+        {
+            cout << "Id Sudah Ada"<< endl;
+        }
+        else
+        {
+            p->next = g.first;
+            g.first = p;
+            cout << "Game Berhasil dibuat" << endl;
+        }
+    }
+}
+
+void createkategori (kategori &k)
+{
+    k.first = NULL;
+}
+
+elmkategori *alokasikategori (infotypekategori x)
+{
+    elmkategori *p = new elmkategori;
+    p->infoGame = x;
+    p->next = NULL;
+    creategame(p->g);
+    return p;
+}
+
+void insertkategori (kategori &k, elmkategori *p)
+{
+    elmkategori *cari = k.first;
+    if (k.first == NULL)
+    {
+        k.first = p;
+        cout << "Kategori Berhasil ditambahkan (1)" << endl;
+    }
+    else if(cari->next == NULL)
+    {
+        if ((p->infoGame.id).compare(cari->infoGame.id) != 0)
+        {
+            p->next = k.first;
+            k.first = p;
+            ;cout << "Kategori Berhasil ditambahkan (2)" << endl;
+        }
+        else
+        {
+            ;cout << "Id Sudah Ada" << endl;
+        }
+    }
+    else
+    {
+        elmkategori *r = k.first;
+        while((r->next != NULL) and ((p->infoGame.id).compare(r->infoGame.id) != 0))
+        {
+            r = r->next;
+        }
+        if ((p->infoGame.id).compare(r->infoGame.id) == 0)
+        {
+            cout << "Id Sudah Ada"<< endl;
+        }
+        else
+        {
+            p->next = k.first;
+            k.first = p;
+            cout << "Kategori Berhasil ditambahkan (3)" << endl;
+        }
+    }
+}
+
+void printinfokat(kategori &a)
+{
+    if (a.first == NULL)
+    {
+        cout << "Kategori belum tersedia" << endl;
+    }
+    else
+    {
+        elmkategori *p = a.first;
+        int num = 1;
+        while(p != NULL)
+        {
+            cout<<num<<". "<<endl;
+            cout<<"Id Kategori: "<< p->infoGame.id<<endl;
+            cout<<"Nama Kategori: "<< p->infoGame.nama<<endl;
+            cout << endl;
+            p = p->next;
+            num++;
+        }
+        cout<<endl;
+    }
+}
+void printinfogame(game &a)
+{
+    if (a.first == NULL)
+    {
+        cout << "Game belum ditamahkan" << endl;
+    }
+    else
+    {
+        elmgame *p = a.first;
+        int num = 1;
+        while(p != NULL)
+        {
+            cout<<num<<". "<<endl;
+            cout<<"Id Game: "<<p->profilgame.id<<endl;
+            cout<<"Judul Game: "<<p->profilgame.title<<endl;
+            cout<<"Rating: "<<p->profilgame.rating<<endl;
+            cout<<"Developer: "<<p->profilgame.developer<<endl;
+            cout << endl;
+            p = p->next;
+            num++;
+        }
+        cout<<endl;
+    }
+}
+void deleteakun(account &a, elmaccount *p)
+{
+
 }
--- ASD.h
+++ ASD.h
@@ -8,6 +8,43 @@
 #define first(P) ((L).first)
 using namespace std;
 
+struct infotypegame
+{
+    string id;
+    string title;
+    double rating;
+    string developer;
+};
+
+struct elmgame
+{
+    infotypegame profilgame;
+    elmgame *next;
+};
+
+struct game
+{
+    elmgame *first;
+    elmgame *last;
+};
+struct infotypekategori
+{
+    string id;
+    string nama;
+};
+
+struct elmkategori
+{
+    infotypekategori infoGame;
+    elmkategori *next;
+    game g;
+};
+
+struct kategori
+{
+    elmkategori *first;
+};
+
 struct infotype
 {
     string id;
@@ -28,10 +65,22 @@ struct account
     elmaccount *first;
 };
 
+void deletegame (game &g, elmgame *p);
+void deleteakun(account &a, elmaccount *p);
+void deletekategori (kategori &k, elmkategori *p);
+void printinfokat(kategori &a);
+void printinfogame(game &a);
+///
 void createaccount(account &a);
 elmaccount *alokasiaccount(infotype x);
 void insertakun(account &a, elmaccount *p);
-void deleteakun(account &a, elmaccount *p);
+
 void printinfo(account &a);
+void creategame (game &g);
+elmgame *alokasigame(infotypegame x);
+void insertgame (game &g, elmgame *p);
+void createkategori (kategori &k);
+elmkategori *alokasikategori (infotypekategori x);
+void insertkategori (kategori &k, elmkategori *p);
 
 #endif // ASD_H_INCLUDED
